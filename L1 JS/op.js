df977fe46e8a04a5d74cef489dbe879215aae047
Aarav-Code-tech <Kartikajay.gupta@gmail.com>
2024-12-03 18:10:20 +0530

    js

---
+++ L1 JS/op.js
@@ -0,0 +1,16 @@
+var a,b,c,d,e,f,g,h;
+a=9;
+b=7;
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+g=a%b;
+h=a**b;
+document.getElementById("ans").innerHTML=c;
+document.getElementById("ans1").innerHTML=d;
+document.getElementById("ans2").innerHTML=e;
+document.getElementById("ans3").innerHTML=f;
+document.getElementById("ans4").innerHTML=g;
+document.getElementById("ans5").innerHTML=h;
+
